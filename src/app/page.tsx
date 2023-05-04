f57e3cf0f90c693746937769b1f13009d7c3534e
Vitor Daniel <vitor036daniel@gmail.com>
2023-05-04 17:53:52 -0300

    add editorjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+"use client"
+
+import { createReactEditorJS } from "react-editor-js"
+// @ts-ignore
+import Header from "@editorjs/header"
+// @ts-ignore
+import CheckList from "@editorjs/checklist"
+// @ts-ignore
+import List from "@editorjs/list"
+// @ts-ignore
+import Paragraph from "@editorjs/paragraph"
+// @ts-ignore
+import Image from "@editorjs/image"
+
 export default function(): JSX.Element {
+  const ReactEditorJS = createReactEditorJS()
+
   return (
-    <h1>Olá mundo!</h1>
+    <ReactEditorJS
+      tools={{
+        header: Header,
+        checklist: CheckList,
+        list: List,
+        paragraph: Paragraph,
+        image: Image
+      }}
+    />
   )
 }
